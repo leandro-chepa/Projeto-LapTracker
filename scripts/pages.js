4a2ff9b0cf26b5b1f60e8bc1390d9948ac569f1c
Leandro Benitti <leandroabenitti@hotmail.com>
2025-06-03 03:23:40 -0300

    first commit

---
+++ scripts/pages.js
@@ -0,0 +1,11 @@
+document.getElementById("btnCadastrarTempo").onclick = function () {
+            window.location.href = "../pages/cadastroTempos.html";
+        };
+
+document.getElementById("btnConsultarTempo").onclick = function () {
+            window.location.href = "../pages/consultarTempos.html";
+        };
+
+document.getElementById("btnTabelaCamp").onclick = function () {
+            window.location.href = "../pages/tabelaCamp.html";
+        };
